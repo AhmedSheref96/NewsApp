9446a8f5ca96e66cdc5d26997cc0e301dd142a1e
Ahmed Sheref <53125308+AhmedSheref96@users.noreply.github.com>
2023-08-16 02:28:12 +0300

    refactor: Navigation Component

---
+++ app/src/main/java/com/el3sas/newsapp/App.kt
@@ -0,0 +1,12 @@
+package com.el3sas.newsapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
