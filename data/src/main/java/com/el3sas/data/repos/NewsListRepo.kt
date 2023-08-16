6d27878e162f84f967aa146b60d998f791e84d45
Ahmed Sheref <53125308+AhmedSheref96@users.noreply.github.com>
2023-08-16 13:36:23 +0300

    refactor: get headlines news list api and repo functionality

--- data/src/main/java/com/el3sas/data/repos/NewsListRepo.kt
+++ data/src/main/java/com/el3sas/data/repos/NewsListRepo.kt
@@ -4,12 +4,13 @@ import com.el3sas.entities.NewsListResponse
 
 interface NewsListRepo {
 
-    suspend fun getNewsList(
-        searchWord: String? = null,
+    suspend fun getFilterableNewsList(
+        searchWord: String,
         dateFrom: String? = null,
         dateTo: String? = null,
         sortBy: String = "publishedAt",
         page: Int? = null
     ): Result<NewsListResponse>
 
+    suspend fun getHeadlineNewsList(country: String, page: Int?): Result<NewsListResponse>
 }
